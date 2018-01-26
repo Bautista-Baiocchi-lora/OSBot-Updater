8b4118ad2e1c8422933fb908bb23ac6282888206
Bautista Baiocchi-Lora <Bautista-Baiocchi-lora@users.noreply.github.com>
2018-01-26 00:02:04 -0300

    saving hooks as json

--- src/org/osbot/updater/analysers/HookFrame.java
+++ src/org/osbot/updater/analysers/HookFrame.java
@@ -1,40 +1,52 @@
 package org.osbot.updater.analysers;
 
+import org.json.simple.JSONObject;
+
 /**
  * Created by Ethan on 1/25/2018.
  */
 public class HookFrame {
-    private String key;
-    private String clazz;
-    private String field;
-    private String returnType;
-    private int paramCount;
+	private String key;
+	private String clazz;
+	private String field;
+	private String returnType;
+	private int paramCount;
 
-    public HookFrame(String key, String clazz, String field, String returnType, int paramCount) {
-        this.key = key;
-        this.clazz = clazz;
-        this.field = field;
-        this.returnType = returnType;
-        this.paramCount = paramCount;
-    }
+	public HookFrame(String key, String clazz, String field, String returnType, int paramCount) {
+		this.key = key;
+		this.clazz = clazz;
+		this.field = field;
+		this.returnType = returnType;
+		this.paramCount = paramCount;
+	}
 
-    public String getKey() {
-        return key;
-    }
+	public String getKey() {
+		return key;
+	}
 
-    public String getClazz() {
-        return clazz;
-    }
+	public String getClazz() {
+		return clazz;
+	}
 
-    public String getField() {
-        return field;
-    }
+	public String getField() {
+		return field;
+	}
 
-    public String getReturnType() {
-        return returnType;
-    }
+	public String getReturnType() {
+		return returnType;
+	}
 
-    public int getParamCount() {
-        return paramCount;
-    }
+	public int getParamCount() {
+		return paramCount;
+	}
+
+	public JSONObject toJson() {
+		JSONObject hookJson = new JSONObject();
+		hookJson.put("Key", getKey());
+		hookJson.put("class", getClazz());
+		hookJson.put("target", getField());
+		hookJson.put("return type", getReturnType());
+		hookJson.put("parameter count", getParamCount());
+		return hookJson;
+	}
 }
--- src/org/osbot/updater/analysers/MainAnalyser.java
+++ src/org/osbot/updater/analysers/MainAnalyser.java
@@ -1,113 +1,138 @@
 package org.osbot.updater.analysers;
 
+import org.json.simple.JSONArray;
+import org.objectweb.asm.tree.ClassNode;
 import org.osbot.updater.analysers.classes.core.ClassAnalyserFrame;
 import org.osbot.updater.analysers.classes.core.ClassCache;
 import org.osbot.updater.main.Loader;
 import org.osbot.updater.misc.Hook;
-import org.objectweb.asm.tree.ClassNode;
+import org.osbot.updater.utils.FileDownloader;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class MainAnalyser {
-    private Map<String, ClassNode> CLASSES = Loader.classArchive.classes;
-    private List<ClassAnalyserFrame> classAnalysers = new ArrayList();
-    private Map<String, HookFrame> hookMap = new HashMap<>();
+	private Map<String, ClassNode> CLASSES = Loader.classArchive.classes;
+	private List<ClassAnalyserFrame> classAnalysers = new ArrayList();
+	private Map<String, HookFrame> hookMap = new HashMap<>();
 
-    private void loadClasses() {
-        this.classAnalysers.add(ClassCache.botApplication);
-        this.classAnalysers.add(ClassCache.accountSettings);
-        this.classAnalysers.add(ClassCache.randomsClass);
-        this.classAnalysers.add(ClassCache.preferenceClass);
-        this.classAnalysers.add(ClassCache.scriptClass);
-        this.classAnalysers.add(ClassCache.scriptCache);
-    }
+	private void loadClasses() {
+		this.classAnalysers.add(ClassCache.botApplication);
+		this.classAnalysers.add(ClassCache.accountSettings);
+		this.classAnalysers.add(ClassCache.randomsClass);
+		this.classAnalysers.add(ClassCache.preferenceClass);
+		this.classAnalysers.add(ClassCache.scriptClass);
+		this.classAnalysers.add(ClassCache.scriptCache);
+	}
 
-    private void runClassAnalysers() {
-        System.out.println("%% Identifying Classes..");
-        for (ClassAnalyserFrame a : this.classAnalysers) {
-            for (ClassNode c : CLASSES.values()) {
-                a.identify(c);
-            }
-            if (a.getName() == null) {
-                Scanner inputClass = new Scanner(System.in);
-                System.out.println(a.getId() + " broke :/");
-                System.out.print(a.getId() + " = ");
-                if (!a.getClass(inputClass.next())) {
-                    System.out.println("Class not found, terminating..");
-                    System.exit(0);
-                }
-            }
-        }
-        System.out.println("    (Identified " + this.classAnalysers.size() + " out of " + this.classAnalysers.size()
-                + " Classes)");
+	private void runClassAnalysers() {
+		System.out.println("%% Identifying Classes..");
+		for (ClassAnalyserFrame a : this.classAnalysers) {
+			for (ClassNode c : CLASSES.values()) {
+				a.identify(c);
+			}
+			if (a.getName() == null) {
+				Scanner inputClass = new Scanner(System.in);
+				System.out.println(a.getId() + " broke :/");
+				System.out.print(a.getId() + " = ");
+				if (!a.getClass(inputClass.next())) {
+					System.out.println("Class not found, terminating..");
+					System.exit(0);
+				}
+			}
+		}
+		System.out.println("    (Identified " + this.classAnalysers.size() + " out of " + this.classAnalysers.size()
+				+ " Classes)");
 
-    }
+	}
 
-    private void runMethodAnalysers() {
+	private void runMethodAnalysers() {
 
-        System.out.println("%% Identifying Fields..");
-        for (ClassAnalyserFrame a : this.classAnalysers) {
-            if (a.hasMethodAnalyser) {
-                a.getMethodAnalyser().identify();
-            }
-        }
-    }
+		System.out.println("%% Identifying Fields..");
+		for (ClassAnalyserFrame a : this.classAnalysers) {
+			if (a.hasMethodAnalyser) {
+				a.getMethodAnalyser().identify();
+			}
+		}
+	}
 
 
-    private void logPrint() {
-        for (ClassAnalyserFrame a : this.classAnalysers) {
-            System.out.print(" # " + a.getId() + ": " + a.getNodes().get(0).name.replace('/','.'));
-            System.out.println();
-            if (a.hasMethodAnalyser) {
-                for (Hook f : a.getMethodAnalyser().getHooks()) {
-                    if(f.getOwner() == null) {
-                     //   System.out.println("     ~> " + f.getId() + " : " + a.getNodes().get(0).name.replace('/','.') + "." + f.getName() + " || "+f.getDesc());
-                        hookMap.put(f.getId(), new HookFrame(f.getId(), a.getNodes().get(0).name.replace("/","."), f.getName(), parseReturn(f.getDesc()), parseParamCount(f.getDesc())));
-                    } else {
-                     //   System.out.println("     ~> " + f.getId() + " : " + f.getOwner().replace('/','.') + "." + f.getName()+ " || "+f.getDesc());
-                        hookMap.put(f.getId(), new HookFrame(f.getId(), f.getOwner().replace('/','.'), f.getName(), parseReturn(f.getDesc()), parseParamCount(f.getDesc())));
+	private void logPrint() {
+		for (ClassAnalyserFrame a : this.classAnalysers) {
+			System.out.print(" # " + a.getId() + ": " + a.getNodes().get(0).name.replace('/', '.'));
+			System.out.println();
+			if (a.hasMethodAnalyser) {
+				for (Hook f : a.getMethodAnalyser().getHooks()) {
+					if (f.getOwner() == null) {
+						//   System.out.println("     ~> " + f.getId() + " : " + a.getNodes().get(0).name.replace('/','.') + "." + f.getName() + " || "+f.getDesc());
+						hookMap.put(f.getId(), new HookFrame(f.getId(), a.getNodes().get(0).name.replace("/", "."), f.getName(), parseReturn(f.getDesc()), parseParamCount(f.getDesc())));
+					} else {
+						//   System.out.println("     ~> " + f.getId() + " : " + f.getOwner().replace('/','.') + "." + f.getName()+ " || "+f.getDesc());
+						hookMap.put(f.getId(), new HookFrame(f.getId(), f.getOwner().replace('/', '.'), f.getName(), parseReturn(f.getDesc()), parseParamCount(f.getDesc())));
 
-                    }
-                }
-            }
-        }
-    }
-    private void printMap() {
-        for(Map.Entry<String, HookFrame> entry : hookMap.entrySet()) {
-            System.out.println("Key: "+entry.getKey()+ " || Class: "+ entry.getValue().getClazz() + " || Field: "
-            + entry.getValue().getField() + " || Return: "+entry.getValue().getReturnType() + " || ParamCount: "+entry.getValue().getParamCount() );
-        }
-    }
-    private int parseParamCount(String desc) {
-        Matcher m = Pattern.compile("\\(([^)]+)\\)").matcher(desc);
-        while(m.find()) {
-            if(m.group(1).contains(";")) {
-                String[] split = m.group(1).split(";");
-                return split.length;
-            }
-        }
-        return 0;
-    }
-    private String parseReturn(String desc) {
-        if(desc.contains(")")) {
-            desc = desc.substring(desc.indexOf(")") + 1);
-            desc = desc.replaceAll(";", "");
-            desc = desc.replace('/', '.').trim();
-            if (desc.substring(0, 1).equals("L")) {
-                desc = desc.replaceFirst("L", "");
-                return desc;
-            }
-        }
-            return "null";
-    }
-    public void run() {
-        loadClasses();
-        runClassAnalysers();
-        runMethodAnalysers();
-        logPrint();
-        printMap();
-    }
+					}
+				}
+			}
+		}
+	}
+
+	private void printMap() {
+		for (Map.Entry<String, HookFrame> entry : hookMap.entrySet()) {
+			System.out.println("Key: " + entry.getKey() + " || Class: " + entry.getValue().getClazz() + " || Field: "
+					+ entry.getValue().getField() + " || Return: " + entry.getValue().getReturnType() + " || ParamCount: " + entry.getValue().getParamCount());
+		}
+	}
+
+	private int parseParamCount(String desc) {
+		Matcher m = Pattern.compile("\\(([^)]+)\\)").matcher(desc);
+		while (m.find()) {
+			if (m.group(1).contains(";")) {
+				String[] split = m.group(1).split(";");
+				return split.length;
+			}
+		}
+		return 0;
+	}
+
+	private String parseReturn(String desc) {
+		if (desc.contains(")")) {
+			desc = desc.substring(desc.indexOf(")") + 1);
+			desc = desc.replaceAll(";", "");
+			desc = desc.replace('/', '.').trim();
+			if (desc.substring(0, 1).equals("L")) {
+				desc = desc.replaceFirst("L", "");
+				return desc;
+			}
+		}
+		return "";
+	}
+
+	private void saveMap() {
+		JSONArray hookArray = new JSONArray();
+		for (HookFrame hook : hookMap.values()) {
+			hookArray.add(hook.toJson());
+		}
+		try (Writer writer = new BufferedWriter(new FileWriter(FileDownloader.getContentDirectory() + "hooks.txt"))) {
+			hookArray.writeJSONString(writer);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println("Saved to hook file.");
+	}
+
+
+	public void run() {
+		loadClasses();
+		runClassAnalysers();
+		runMethodAnalysers();
+		logPrint();
+		printMap();
+		saveMap();
+	}
 
 }
